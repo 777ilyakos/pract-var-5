de5caeb92d9db6e553bc8e0883f6144893facd3a
777ilyakos <91250999+777ilyakos@users.noreply.github.com>
2021-11-03 07:24:19 +0300

    перемещено всё необходимое из практической 6 в 7

--- 7/7/InformationWindow.xaml.cs
+++ 7/7/InformationWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace _6
+namespace _7
 {
     /// <summary>
     /// Логика взаимодействия для InformationWindow.xaml
