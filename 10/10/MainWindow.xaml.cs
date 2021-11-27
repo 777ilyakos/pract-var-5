5fc93e53d547b6d45ac11b7df31ea334666d6ffb
777ilyakos <91250999+777ilyakos@users.noreply.github.com>
2021-11-27 09:20:25 +0300

    работает 9

--- 10/10/MainWindow.xaml.cs
+++ 10/10/MainWindow.xaml.cs
@@ -22,14 +22,20 @@ namespace _10
         {
             InitializeComponent();
             listItem.ItemsSource = _array;
-
+            AddRandomIntInCollection(_array, 100, -5, 5);
+            _array.Add(0);
+            _array.Sort();
+            int zero1 = _array.IndexOf(0);
+            int zero2 = _array.LastIndexOf(0);
+            MessageBox.Show(zero1 + " < 0 \n"+(_array.Count- zero2-1) +" > 0");
+            _array.RemoveAt(zero1);
         }
         public void AddRandomIntInCollection(ICollection<int> collection, int count, int minValue, int maxValue)
         {
             Random random = new Random();
             for (int i = 0; i < count; i++)
             {
-                collection.Add(random.Next(minValue, maxValue));
+                collection.Add(random.Next(minValue, maxValue+1));
             }
         }
     }
