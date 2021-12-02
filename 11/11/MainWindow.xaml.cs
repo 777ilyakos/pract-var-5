36b5f88723dced49af5e39508a900484cec6e381
исп-31 <-31@LAB24-12>
2021-12-02 15:33:41 +0300

    сделал

--- 11/11/MainWindow.xaml.cs
+++ 11/11/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace _11
 {
@@ -23,6 +24,30 @@ namespace _11
         public MainWindow()
         {
             InitializeComponent();
+            string s1 = "aa aba abba abbba abca abea";
+            Regex regex1 = new Regex(@"ab{0,}a");
+            MatchCollection matchs1 = regex1.Matches(s1);
+            StringBuilder oneString1 = new StringBuilder();
+            for (int i = 0; i < matchs1.Count; i++)
+            {
+                oneString1.AppendLine(matchs1[i].Value.ToString());
+            }
+            MessageBox.Show(oneString1.ToString());
+
+
+
+            string s2 = "a1a a2a a3a a4a a5a aba aca";
+            Regex regex2 = new Regex(@"a[0-9]a");
+            MatchCollection matchs2 = regex2.Matches(s2);
+            StringBuilder oneString2 = new StringBuilder();
+            for (int i = 0; i < matchs2.Count; i++)
+            {
+                oneString2.AppendLine(matchs2[i].Value.ToString());
+            }
+            MessageBox.Show(oneString2.ToString());
+
+
+            this.Close();
         }
     }
 }
